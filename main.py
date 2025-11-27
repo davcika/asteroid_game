a2834eb771afd042ec975b8adc3b2e895b9a7342
davcika <daviskovals6@gmail.com>
2025-11-28 00:44:33 +0200

    added triangle polygon

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width: 1280")
     print("Screen height: 720")
@@ -15,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
